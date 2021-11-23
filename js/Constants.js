d0d63d49fb578b4136981d56adb75a3705d9bc6a
Karthik Padav <karthik.padav@tecnotree.com>
2021-11-23 15:22:19 +0530

    Google analytics

--- js/Constants.js
+++ js/Constants.js
@@ -1,6 +1,7 @@
 const constants = {
   SITE_TITLE: "Watch Movies Online",
   SITE_URL: process.env.SITE_URL,
+  GOOGLE_ANALYTICS: process.env.GOOGLE_ANALYTICS,
   imageBaseUrl: "https://image.tmdb.org/t/p/original",
   THEME: {
     DARK: "dark",
